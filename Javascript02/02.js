25c661f07c89fbebec038f0552b0167e678185fd
Kunal <kunkashyap4@gmail.com>
2026-02-03 12:05:18 +0530

    Arranged files and changed the color inside the paragraph.

---
+++ Javascript02/02.js
@@ -0,0 +1,8 @@
+console.log("Heyy!")
+const para= document.getElementsByClassName('a')
+para[0].innerText = "This is updated paragraph"
+para[1].style.color="Maroon"
+console.log(para)
+
+// Summary: changing colors of text by selecting the class of object <p>
+// with the use of style.color="<color-name>"
