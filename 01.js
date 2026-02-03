5368918ecf1c1d94e69f4b72a9705efd4d69205f
Kunal <kunkashyap4@gmail.com>
2026-02-03 11:53:26 +0530

    Summary: with querySelector(<class/div>) u can manipulate the data inside HTML

---
+++ 01.js
@@ -0,0 +1,7 @@
+console.log("Bonjour!")
+console.dir(document)
+const para=document.querySelector('p')
+para.innerText="Au revoir!"
+
+
+//Summary: with querySelector(<class/div>) u can manipulate the data inside HTML
