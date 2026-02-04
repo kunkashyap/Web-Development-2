a8f56f2b92100f0d1e1a27fe92b89a2e2f6760a5
Kunal <kunkashyap4@gmail.com>
2026-02-04 15:03:57 +0530

    .then()

---
+++ Javascript06/06.js
@@ -0,0 +1,46 @@
+function doHomework(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let homeworkDone = true;
+            if (homeworkDone){
+                console.log("Homework is done");
+                resolve("Homework complete");
+
+            }else{
+                reject("Homework not done.");
+            }
+        }
+    ,3000);
+})
+}
+
+
+function eatDinner(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let dinnerEaten = true;
+            if (dinnerEaten){
+                console.log("Dinner is done");
+                resolve("Dinner complete");
+
+            }else{
+                reject("Dinner not done.");
+            }
+        },2000);
+});
+}
+
+function gotoPlayground(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let permission = true;
+            if (permission){
+                console.log("Went to playground.");
+                resolve("Playground time!");
+
+            }else{
+                reject("Not allowed to go.");
+            }
+        },3000);
+});
+}
